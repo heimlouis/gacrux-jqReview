505ae2426854792dfabfe8c90f1a09c88aeab5b7
devjanaprime <dev@primeacademy.io>
2018-02-01 11:29:14 -0600

    end of morning lecture

--- jqReview.js
+++ jqReview.js
@@ -1,3 +1,5 @@
+var garage = [];
+
 $( document ).ready( function(){
   $( '#clickyButton' ).on( 'click', function(){
     console.log( 'in clickyButton on click' );
@@ -8,8 +10,31 @@ $( document ).ready( function(){
       make: $( '#makeIn' ).val(),
       model: $( '#modelIn' ).val()
     }
-    console.log( newCar );
     // push that car into our garage
+    garage.push( newCar );
     // display what is in our garage
+    displayGarage();
+    // empty inputs
+    $( '#yearIn' ).val( '' );
+    $( '#makeIn' ).val( '' );
+    $( '#modelIn' ).val( '' );
   }); // end clickyButton on click
 }); //end doc ready
+
+function displayGarage(){
+  console.log( 'in displayGarage' );
+  // target output element by ID
+  var output = $( '#garageOut' );
+  // empty output element
+  output.empty();
+  // loop though cars in garage and display on DOM
+  for( var i=0; i<garage.length ; i++ ){
+    // append each car into output element
+    var outputString = '<li>';
+    outputString += garage[ i ].year + ' ';
+    outputString += garage[ i ].make + ' ';
+    outputString += garage[ i ].model;
+    outputString += '</li>';
+    output.append( outputString );
+  } //end for
+} // end displayGarage
