4e9839e1a5ca4c6c8b703ee53b6d395ab1963eaf
devjanaprime <dev@primeacademy.io>
2018-02-01 11:10:05 -0600

    user input into object

--- jqReview.js
+++ jqReview.js
@@ -1,5 +1,15 @@
 $( document ).ready( function(){
   $( '#clickyButton' ).on( 'click', function(){
     console.log( 'in clickyButton on click' );
+    // capture user input
+    // create an object with the user input
+    var newCar = {
+      year: $( '#yearIn' ).val(),
+      make: $( '#makeIn' ).val(),
+      model: $( '#modelIn' ).val()
+    }
+    console.log( newCar );
+    // push that car into our garage
+    // display what is in our garage
   }); // end clickyButton on click
 }); //end doc ready
